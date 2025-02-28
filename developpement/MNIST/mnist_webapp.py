071e9a6487030f8fd03c9d38fae5e42bdc1e49f3
Metriida <mathferdu45@gmail.com>
2025-02-28 10:59:10 +0100

    tp1 first commit

--- developpement/MNIST/mnist_webapp.py
+++ developpement/MNIST/mnist_webapp.py
@@ -2,15 +2,26 @@ import gradio as gr
 from PIL import Image
 import requests
 import io
+import numpy as np
+import argparse
+import torch
+from model import MNISTNet
+import matplotlib.pyplot as plt
+
+
 
 
 def recognize_digit(image):
     # Convert to PIL Image necessary if using the API method
+    image = image['composite']    
+    image = image[:,:,3]
     image = Image.fromarray(image.astype('uint8'))
-    img_binary = ...
-    ...
-    ...
-    return ....
+    img_binary = io.BytesIO()
+    image.save(img_binary, format="PNG")
+
+    response = requests.post("http://127.0.0.1:5000/predict", data=img_binary.getvalue())
+    predicted_label = response.json()["prediction"]
+    return predicted_label
 
 if __name__=='__main__':
 
@@ -19,4 +30,4 @@ if __name__=='__main__':
                 outputs='label',
                 live=True,
                 description="Draw a number on the sketchpad to see the model's prediction.",
-                ).launch(debug=True, share=True);
+                ).launch(debug=True, share=True)
--- developpement/MNIST/train.py
+++ developpement/MNIST/train.py
@@ -8,13 +8,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
+from torchsummary import summary
+from torch.utils.tensorboard import SummaryWriter
 
 from model import MNISTNet
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
- # setting device on GPU if available, else CPU
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-def train(net, optimizer, loader, epochs=10):
+def train(net, optimizer, loader, writer, epochs=10):
     criterion = nn.CrossEntropyLoss()
     for epoch in range(epochs):
         running_loss = []
@@ -28,6 +29,7 @@ def train(net, optimizer, loader, epochs=10):
             loss.backward()
             optimizer.step()
             t.set_description(f'training loss: {mean(running_loss)}')
+        writer.add_scalar('training loss', mean(running_loss), epoch)
 
 def test(model, dataloader):
     test_corrects = 0
@@ -41,3 +43,57 @@ def test(model, dataloader):
             total += y.size(0)
     return test_corrects / total
 	
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', type=str, default='mnist')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=10)
+    args = parser.parse_args()
+
+    exp_name = args.exp_name
+    batch_size = args.batch_size
+    lr = args.lr
+    epochs = args.epochs
+
+    writer = SummaryWriter(f'runs/MNIST/{exp_name}')
+
+    # transforms
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))])
+
+    # datasets
+    trainset = torchvision.datasets.MNIST('./data', download=True, train=True, transform=transform)
+    testset = torchvision.datasets.MNIST('./data', download=True, train=False, transform=transform)
+
+    # dataloaders
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    net = MNISTNet().to(device)
+    # print summary of the model
+    summary(net, (1, 28, 28))
+    optimizer = optim.SGD(net.parameters(), lr=lr)
+    train(net, optimizer, trainloader, writer, epochs=epochs)
+    test_acc = test(net, testloader)
+    print(f'Test accuracy: {test_acc}')
+
+    torch.save(net.state_dict(), 'weights/mnist_net.pth')
+
+    #add embeddings to tensorboard
+    perm = torch.randperm(len(trainset.data))
+    images, labels = trainset.data[perm][:256], trainset.targets[perm][:256]
+    images = images.unsqueeze(1).float().to(device)
+    with torch.no_grad():
+        embeddings = net.get_features(images)
+        writer.add_embedding(embeddings,
+                        metadata=labels,
+                        label_img=images, global_step=1)
+
+        # save networks computational graph in tensorboard
+        writer.add_graph(net, images)
+        # save a dataset sample in tensorboard
+        img_grid = torchvision.utils.make_grid(images[:64])
+        writer.add_image('mnist_images', img_grid)
